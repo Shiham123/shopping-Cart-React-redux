0beef486e32e094b9b0dd067393efc01e76953cc
Shiham123 <s36651786@gmail.com>
2023-12-18 11:07:16 +0600

    auth context

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
+import { useSelector } from 'react-redux';
 import Auth from './components/Auth';
 import Layout from './components/Layout';
 
 function App() {
+  const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
   return (
     <div>
-      {/* <Auth /> */}
-      <Layout />
+      {!isLoggedIn && <Auth />}
+      {isLoggedIn && <Layout />}
     </div>
   );
 }
